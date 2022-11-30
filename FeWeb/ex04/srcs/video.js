92e41edceac0b181a8785a223d84285777ac02ac
jpham005 <jpham005@naver.com>
2022-11-30 21:20:29 +0900

    innerHTML

--- FeWeb/ex04/srcs/video.js
+++ FeWeb/ex04/srcs/video.js
@@ -4,5 +4,5 @@ const additionalContent = document.querySelector('.additionalContent');
 
 moreButton.addEventListener('mouseup', () => {
   moreButton.style.display = 'none';
-  additionalContent.removeAttribute('hidden');
+  content.innerHTML = content.innerHTML + '<br/>' + additionalContent.innerHTML;
 });
