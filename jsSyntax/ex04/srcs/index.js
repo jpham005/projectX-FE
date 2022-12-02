066cc2c91e627e85af21f8d3b2005733a97cb025
jpham005 <jpham005@naver.com>
2022-12-02 16:50:16 +0900

    04

---
+++ jsSyntax/ex04/srcs/index.js
@@ -0,0 +1,25 @@
+const Person = {
+  name: 'jaham',
+  age: 26,
+  attr: 'member'
+};
+
+function makeEmployee(name, age, sabun) {
+  return {
+    name,
+    age,
+    sabun,
+  }
+}
+
+const child = new Person;
+child.something = 1;
+
+class Student {
+ constructor(
+   this
+ )
+}
+
+const hi = new Student;
+
